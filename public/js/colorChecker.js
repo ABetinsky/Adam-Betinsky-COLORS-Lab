bc20fd7286b4a866800b2c5db777059a6e838f40
Adam Betinsky <adambetinskyucf@gmail.com>
2026-04-24 13:28:35 -0400

    Added helper function file for colorChecker test

---
+++ public/js/colorChecker.js
@@ -0,0 +1,6 @@
+// Code written by Adam Betinsky on April 24th, 2026
+// This function is a simple checker to see replicate checking if a input to a text box on my site is empty. This is used for Assignment 5
+export function checkIfInputNotEmpty (input)
+{
+    return input.trim().length > 0;
+}
