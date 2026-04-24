3cfb092fe51f57aced94ce8f8ff28d992da5e488
Adam Betinsky <adambetinskyucf@gmail.com>
2026-04-24 13:44:13 -0400

    Made jest run in module mode to work on frontend test. Also renamed test files. Should be done

--- frontend_unitTest.test.js
+++ frontend_unitTest.test.js
@@ -4,6 +4,6 @@ import { checkIfInputNotEmpty } from "./public/js/colorChecker.js";
 // This is my frontend unit test. It ensures that an input entered into a textbox is NOT empty (this uses a replicator function in my colorChecker.js file)
 test ("Ensure color inputted to textbox is NOT empty", () => {
     // Try testing with a working case and not a working case
-    expect(checkIfInputNotEmpty("Blue").toBe(true));
-    expect(checkIfInputNotEmpty("").toBe(false));
+    expect(checkIfInputNotEmpty("Blue")).toBe(true);
+    expect(checkIfInputNotEmpty("")).toBe(false);
 });
